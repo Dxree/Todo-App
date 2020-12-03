5c7b33ff569b501c32798ea55ee9319c18a55f88
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-03 21:58:52 +0000

    Deploy Dxree/Todo-App to github.com/Dxree/Todo-App.git:gh-pages

--- src/app/user.model.ts
+++
@@ -1,9 +0,0 @@
-import {Task} from './task.model';
-import {Category} from './category.model';
-
-export interface User {
-  username?: string; // must be unique
-  password?: string;
-  tasks?: Task[];
-  categories?: Category[];
-}
