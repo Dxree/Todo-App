f6c6cfadcd3806e1fc8e300f2c934fa6c74ab2ef
bift85 <britta.fichtl@mni.thm.de>
2020-12-06 22:50:47 +0100

    tests for login component done

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, inject, TestBed, tick} from '@angular/core/testing';
 
 import {LoginComponent} from './login.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
@@ -6,13 +6,15 @@ import {AngularFireAuthModule} from '@angular/fire/auth';
 import {AngularFirestore, AngularFirestoreModule} from '@angular/fire/firestore';
 import {AngularFireModule} from '@angular/fire';
 import {firebaseConfig} from '../app.module';
-import { RouterModule} from '@angular/router';
+import {Router, RouterModule} from '@angular/router';
 import {routes} from '../app-routing.module';
 import {GetListComponent} from '../get-list/get-list.component';
 import {RegisterComponent} from '../register/register.component';
 import {AddTaskComponent} from '../add-task/add-task.component';
 import {AddCategoryComponent} from '../add-category/add-category.component';
 import {NgbModule, NgbNavModule} from '@ng-bootstrap/ng-bootstrap';
+import {Location} from '@angular/common';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -29,7 +31,7 @@ describe('LoginComponent', () => {
         AngularFireAuthModule,
         AngularFireModule.initializeApp(firebaseConfig),
         AngularFirestoreModule,
-        RouterModule.forRoot(routes)
+        [RouterTestingModule.withRoutes(routes)]
       ],
       providers: [
         AngularFirestore,
@@ -48,4 +50,37 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render title', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Anmeldung');
+  });
+
+  it('should render input field for username and for password', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#username')).toBeTruthy();
+    expect(compiled.querySelector('#password')).toBeTruthy();
+  });
+
+  it('on click of button "registerSubmitBtn" onSubmit() should be called', fakeAsync(() => {
+      const compiled = fixture.debugElement.nativeElement;
+      spyOn(component, 'onSubmit');
+      const button = compiled.querySelector('#loginSubmitBtn');
+      button.click();
+      tick();
+      fixture.detectChanges();
+      expect(component.onSubmit).toHaveBeenCalled();
+    })
+  );
+
+  it('on click on router link "Registrieren" you should be on register page', fakeAsync((
+    inject([Router, Location], (router: Router, location: Location) => {
+        const compiled = fixture.debugElement.nativeElement;
+        const routerLink = compiled.querySelector('#register-link');
+        routerLink.click();
+        tick();
+        expect(router.url).toBe('/register');
+      }
+    )
+  )));
 });
