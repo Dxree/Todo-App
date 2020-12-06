6077f6e53e540f8a390ff08cd1b758a5ec32836e
ChEb <christiana.eberle@mni.thm.de>
2020-12-06 19:37:09 +0100

    added imports and provider

--- src/app/task.service.spec.ts
+++ src/app/task.service.spec.ts
@@ -1,9 +1,22 @@
 import {TestBed} from '@angular/core/testing';
+import {firebaseConfig} from './app.module';
 
 import {TaskService} from './task.service';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestore, AngularFirestoreModule} from '@angular/fire/firestore';
 
 describe('TaskService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      AngularFireAuthModule,
+      AngularFireModule.initializeApp(firebaseConfig),
+      AngularFirestoreModule
+    ],
+    providers: [
+      AngularFirestore
+    ]
+  }));
 
   it('should be created', () => {
     const service: TaskService = TestBed.get(TaskService);
