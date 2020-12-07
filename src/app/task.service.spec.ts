6e2557983e0ade88e6f7437f34416ddf6c78ee4a
ChEb <christiana.eberle@mni.thm.de>
2020-12-08 00:09:37 +0100

    some more tests (wip)

--- src/app/task.service.spec.ts
+++ src/app/task.service.spec.ts
@@ -6,10 +6,24 @@ import {AngularFireAuthModule} from '@angular/fire/auth';
 import {AngularFireModule} from '@angular/fire';
 import {AngularFirestore, AngularFirestoreModule} from '@angular/fire/firestore';
 import * as firebase from 'firebase';
+import {Category} from './category.model';
+import Timestamp = firebase.firestore.Timestamp;
+import { Task } from './task.model';
 
 describe('TaskService', () => {
 
   const testUsername = 'user-for-taskService-test';
+  const categoryModel: Category = {
+    title: 'kms'
+  };
+  const taskModel: Task = {
+    title: 'create more Tasks',
+    description: '...',
+    done: false,
+    priority: 3,
+    categories: [categoryModel],
+    deadline: Timestamp.fromDate(new Date(2020, 12, 24))
+  };
   let service: TaskService;
   let userCollectionRef;
   let userTaskRef;
@@ -31,7 +45,50 @@ describe('TaskService', () => {
     userTaskRef =  userCollectionRef.doc(testUsername).collection('tasks');
   });
 
+  afterEach(async () => {
+    await service.clearTaskList(testUsername);
+    await userCollectionRef.doc(testUsername).delete();
+  });
+
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should save and fetch new task', async () => {
+    const task = taskModel;
+    const success: boolean = await service.addTask(testUsername, task);
+    expect(success).toBeTruthy();
+
+    // expect(await service.getAllTasks(testUsername)).toContain(task);
+  });
+
+  /*
+  it('should update tasks', async () => {
+    let task: Task = taskModel;
+    await userTaskRef.add(task).then(document => {
+      task = document;
+    });
+
+    // update
+    task.title = 'task was updated';
+    const success: boolean = await service.updateTask(testUsername, task);
+    expect(success).toBeTruthy();
+
+    let data;
+    await userTaskRef.doc(task.id).get().then(doc => data = doc.data());
+    expect(data.title).toEqual(task.title);
+  });*/
+
+  it('should delete tasks', async () => {
+    const task = taskModel;
+    const tasksRef = firebase.firestore().collection('users').doc(testUsername).collection('tasks');
+    await tasksRef.add(task).then(document => {
+      task.id = document.id;
+    });
+
+    // delete
+    await service.deleteTask(testUsername, task);
+    await expect(await service.getAllTasks(testUsername)).not.toContain({id: task.id});
+  });
+
 });
