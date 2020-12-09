a08beab1a89871e8285287d656428335cd2c5f16
bift85 <britta.fichtl@mni.thm.de>
2020-12-09 12:18:36 +0100

    fixed quotation marks

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,15 @@
-import {TestBed, async} from '@angular/core/testing';
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import {AppComponent} from './app.component';
-import {UserService} from "./user.service";
-import {User} from "./user.model";
+import {UserService} from './user.service';
+import {User} from './user.model';
 
 describe('AppComponent', () => {
   let componentUserService: UserService;
   let userService: UserService;
   let userServiceStub: Partial<UserService>;
+
+  let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(async(() => {
     userServiceStub = {
@@ -30,16 +32,20 @@ describe('AppComponent', () => {
     },
       ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    userService = fixture.debugElement.injector.get(UserService);
+    componentUserService = userService;
+    userService = TestBed.inject(UserService);
+    fixture.detectChanges();
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'Todo-List'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Todo-List');
   });
